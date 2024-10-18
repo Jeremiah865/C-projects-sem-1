60b69c51d4afa2460ca4a6a6b230801044bd912e
Jeremiah865 <jeremiahneepe@gmail.com>
2024-10-18 11:05:31 +0300

    Add files via upload

---
+++ 1D array.c
@@ -0,0 +1,13 @@
+//c array-1D array;
+/*Author:jeremiah
+Reg no:23223/24
+*/
+#include<stdio.h>
+int i;
+int main (){
+    int marks[5]={50,60,70,80,2};
+    for(i=0;i<5;i++){
+        printf("marks[%d]=%d\n",i,marks[i]);
+    }
+
+}
